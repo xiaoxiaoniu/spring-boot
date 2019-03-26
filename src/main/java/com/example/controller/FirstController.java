70fb1c9d93a7721fcb79b6fc2b4b7165271edf06
sunshiyu <sunshiyu@richinfo.cn>
2019-03-26 14:46:24 +0800

    next commit

---
+++ src/main/java/com/example/controller/FirstController.java
@@ -0,0 +1,21 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * TODO:
+ *
+ * @ClassName: FirstController
+ * @author: sunshiyu@richinfo.cn
+ * @Date: 2019/3/26 14:43
+ **/
+@RestController
+@RequestMapping("/first")
+public class FirstController {
+
+  @RequestMapping("/method1")
+  public String method1() {
+    return "hi man!";
+  }
+}
